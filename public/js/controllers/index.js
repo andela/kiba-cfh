a04880f488c589e98473e3317838d18bd5210ac7
andela-eefekemo <eguonoghene.efekemo@andela.com>
2017-06-02 01:17:16 +0100

    integrate landing page

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -76,5 +76,85 @@ angular.module('mean.system').controller('IndexController', [
     AvatarService.getAvatars().then((data) => {
       $scope.avatars = data;
     });
+    $scope.isNewUser = () => $window.user && $window.user.isNewUser;
+    $scope.load = function () {
+      const onScrollAnimations = () => {
+        let inview;
+        if ($('.img1').length) {
+          inview = new Waypoint.Inview({
+            element: $('.img1')[0],
+            entered() {
+              $('.img1').addClass('slideIn1');
+            }
+          });
+          console.log('i am me');
+        }
+        inview = new Waypoint.Inview({
+          element: $('.img2')[0],
+          entered() {
+            $('.img2').addClass('slideIn2');
+          }
+        });
+        inview = new Waypoint.Inview({
+          element: $('.img3')[0],
+          entered() {
+            $('.img3').addClass('slideIn3');
+          }
+        });
+
+        inview = new Waypoint.Inview({
+          element: $('.text1')[0],
+          entered() {
+            $('.text1').addClass('bounceIn1');
+          },
+          exited() {
+            // $('.text1').removeClass('bounceIn1');
+          }
+        });
+        inview = new Waypoint.Inview({
+          element: $('.text2')[0],
+          entered() {
+            $('.text2').addClass('bounceIn2');
+          },
+          exited() {
+            // $('.text2').removeClass('bounceIn2');
+          }
+        });
+        inview = new Waypoint.Inview({
+          element: $('.text3')[0],
+          entered() {
+            $('.text3').addClass('bounceIn3');
+          },
+          exited() {
+            // $('.text3').removeClass('bounceIn3');
+          }
+        });
+        inview = new Waypoint.Inview({
+          element: $('.wp1')[0],
+          entered() {
+            $('.wp1').addClass('slideInLeft');
+          }
+        });
+        inview = new Waypoint.Inview({
+          element: $('.wp2')[0],
+          entered() {
+            $('.wp2').addClass('slideInRight');
+          }
+        });
+      };
+      $(document).ready(() => {
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+        $('#download-button').click(() => {
+          $('html, body').animate(
+            {
+              scrollTop: $('.intro').offset().top
+            },
+            2000
+          );
+        });
+        onScrollAnimations();
+      });
+    };
   }
 ]);
