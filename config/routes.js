72315d9e970c47c17efd0550ba0dd272b9078152
Johnbosco Ohia <johnbosco.ohia@andela.com>
2017-06-06 12:38:40 +0100

    #144937471 Cards should be categorized by region (#45)
    * feature(invites): send users invites
    game owner should be able to send invites to a maximum of 11 other users
    [Finishes #144937455]
    
    * feature(invites): refactor code for user invites
    [Finishes #144937455]
    
    * feature(localization): categorize cards by region
    
    [Finishes #144937471]
    
    * feature(localization): categorize cards by region
    
    [Finishes #144937471]
    
    * feature(localization): fix selecting card by region
    
    [Finishes #144937471]
    
    * feature(localization): delete mail.js and invite.js file from branch
    
    [Finishes #144937471]
    
    * feature(localization): fix error on start game  [Finishes #144937471]
    
    * feature(localization): create a region.json file and fix edge cases on start game  [Finishes #144937471]
    
    * feature(localization): code refactoring  [Finishes #144937471]
    
    * feature(localization): fix routing for questions and answers
    
    [Finishes #144937471]

--- config/routes.js
+++ config/routes.js
@@ -1,3 +1,4 @@
+let async = require('async');
 const jwt = require('./jwt');
 const users = require('../app/controllers/users');
 const answers = require('../app/controllers/answers');
@@ -6,20 +7,23 @@ const avatars = require('../app/controllers/avatars');
 const index = require('../app/controllers/index');
 
 module.exports = (app, passport, auth) => {
+  // region Route
+  app.get('/region.json', (req, res) => {
+    return res.json(require('./region.json'));
+  });
   // User Routes
   app.get('/signin', users.signin);
   app.get('/signup', users.signup);
   app.get('/chooseavatars', users.checkAvatar);
   app.get('/signout', users.signout);
-
+  app.get('/users', users.all);
   // Setting up the users api
   app.post('/users', users.create);
   app.post('/users/avatars', users.avatars);
 
   // Donation Routes
   app.post('/donations', users.addDonation);
-
-  app.post(
+app.post(
     '/users/session',
     passport.authenticate('local', {
       failureRedirect: '/signin',
@@ -108,19 +112,28 @@ module.exports = (app, passport, auth) => {
   app.param('userId', users.user);
 
   // Answer Routes
-  app.get('/answers', answers.all);
+  app.get('/answers/', answers.all);
   app.get('/answers/:answerId', answers.show);
+  app.post('/api/answers/:region', answers.byRegion);
   // Finish with setting up the answerId param
   app.param('answerId', answers.answer);
 
   // Question Routes
-  app.get('/questions', questions.all);
+  app.get('/questions/', questions.all);
   app.get('/questions/:questionId', questions.show);
+  app.post('/api/questions/:region', questions.byRegion);
+
   // Finish with setting up the questionId param
   app.param('questionId', questions.question);
 
   // Avatar Routes
+  let avatars = require('../app/controllers/avatars');
   app.get('/avatars', avatars.allJSON);
+
+  // Home route
+  let index = require('../app/controllers/index');
+  app.get('/play', index.play);
+  app.get('/', index.render);
 
   // Home route
   app.get('/play', index.play);
@@ -129,4 +142,5 @@ module.exports = (app, passport, auth) => {
   // JWT API endpoint
   app.post('/api/auth/login', jwt.SignInWithJwt);
   app.post('/api/auth/signup', jwt.SignUpWithJwt);
+
 };
