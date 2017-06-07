d1b13bfc08621c69444af773bcd1afaeef1c7a35
Johnbosco Ohia <johnbosco.ohia@andela.com>
2017-06-07 12:54:02 +0100

    feature(communities): emojis for chat window
    [Finishes #144937479]

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ngRoute', 'mean.system', 'mean.directives', 'ngSanitize', 'ngAnimate'])
+angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ngRoute', 'mean.system', 'mean.directives', 'ngSanitize', 'ngAnimate', 'toastr'])
   .config(['$routeProvider',
       function($routeProvider) {
           $routeProvider.
