508aad5b49610b4e7a194e61c99271277f9b2f3a
andela-eefekemo <eguonoghene.efekemo@andela.com>
2017-06-15 12:53:05 +0100

    intergrate gamelogs

--- public/js/directives.js
+++ public/js/directives.js
@@ -2,75 +2,75 @@
 /* global window */
 
 angular.module('mean.directives', [])
-  .directive('player', function (){
-    return{
+  .directive('player', function () {
+    return {
       restrict: 'EA',
       templateUrl: '/views/player.html',
-      link: function(scope, elem, attr){
+      link: function (scope, elem, attr) {
         scope.colors = ['#7CE4E8', '#FFFFa5', '#FC575E', '#F2ADFF', '#398EC4', '#8CFF95'];
       }
     };
-  }).directive('answers', function() {
+  }).directive('answers', function () {
     return {
       restrict: 'EA',
       templateUrl: '/views/answers.html',
-      link: function(scope, elem, attr) {
-        scope.$watch('game.state', function() {
+      link: function (scope, elem, attr) {
+        scope.$watch('game.state', function () {
           if (scope.game.state === 'winner has been chosen') {
             var curQ = scope.game.curQuestion;
             var curQuestionArr = curQ.text.split('_');
-            var startStyle = "<span style='color: "+scope.colors[scope.game.players[scope.game.winningCardPlayer].color]+"'>";
+            var startStyle = "<span style='color: " + scope.colors[scope.game.players[scope.game.winningCardPlayer].color] + "'>";
             var endStyle = '</span>';
             var shouldRemoveQuestionPunctuation = false;
-            var removePunctuation = function(cardIndex) {
+            var removePunctuation = function (cardIndex) {
               var cardText = scope.game.table[scope.game.winningCard].card[cardIndex].text;
-              if (cardText.indexOf('.',cardText.length-2) === cardText.length-1) {
-                cardText = cardText.slice(0,cardText.length-1);
-              } else if ((cardText.indexOf('!',cardText.length-2) === cardText.length-1 ||
-                cardText.indexOf('?',cardText.length-2) === cardText.length-1) &&
-                cardIndex === curQ.numAnswers-1) {
+              if (cardText.indexOf('.', cardText.length - 2) === cardText.length - 1) {
+                cardText = cardText.slice(0, cardText.length - 1);
+              } else if ((cardText.indexOf('!', cardText.length - 2) === cardText.length - 1 ||
+                cardText.indexOf('?', cardText.length - 2) === cardText.length - 1) &&
+                cardIndex === curQ.numAnswers - 1) {
                 shouldRemoveQuestionPunctuation = true;
               }
               return cardText;
             };
             if (curQuestionArr.length > 1) {
               var cardText = removePunctuation(0);
-              curQuestionArr.splice(1,0,startStyle+cardText+endStyle);
+              curQuestionArr.splice(1, 0, startStyle + cardText + endStyle);
               if (curQ.numAnswers === 2) {
                 cardText = removePunctuation(1);
-                curQuestionArr.splice(3,0,startStyle+cardText+endStyle);
+                curQuestionArr.splice(3, 0, startStyle + cardText + endStyle);
               }
               curQ.text = curQuestionArr.join('');
               // Clean up the last punctuation mark in the question if there already is one in the answer
               if (shouldRemoveQuestionPunctuation) {
-                if (curQ.text.indexOf('.',curQ.text.length-2) === curQ.text.length-1) {
-                  curQ.text = curQ.text.slice(0,curQ.text.length-2);
+                if (curQ.text.indexOf('.', curQ.text.length - 2) === curQ.text.length - 1) {
+                  curQ.text = curQ.text.slice(0, curQ.text.length - 2);
                 }
               }
             } else {
-              curQ.text += ' '+startStyle+scope.game.table[scope.game.winningCard].card[0].text+endStyle;
+              curQ.text += ' ' + startStyle + scope.game.table[scope.game.winningCard].card[0].text + endStyle;
             }
           }
         });
       }
     };
-  }).directive('question', function() {
+  }).directive('question', function () {
     return {
       restrict: 'EA',
       templateUrl: '/views/question.html',
-      link: function(scope, elem, attr) {}
+      link: function (scope, elem, attr) { }
     };
   })
-  .directive('timer', function(){
-    return{
-      restrict: 'EA',
-      templateUrl: '/views/timer.html',
-      link: function(scope, elem, attr){}
-    };
-  }).directive('landing', function() {
+  .directive('timer', function () {
     return {
       restrict: 'EA',
-      link: function(scope, elem, attr) {
+      templateUrl: '/views/timer.html',
+      link: function (scope, elem, attr) { }
+    };
+  }).directive('landing', function () {
+    return {
+      restrict: 'EA',
+      link: function (scope, elem, attr) {
         scope.showOptions = true;
 
         if (scope.$$childHead.global.authenticated === true) {
@@ -79,11 +79,107 @@ angular.module('mean.directives', [])
       }
     };
   })
+  .directive('dashboardTabs', () => {
+    return {
+      restrict: 'EA',
+      transclude: true,
+      scope: {},
+      controller: ['$scope', function ($scope) {
+        const panes = [];
+        $scope.select = (pane) => {
+          angular.forEach(panes, (currentPane) => {
+            currentPane.selected = false;
+          });
+          pane.selected = true;
+        };
+        this.addPane = (pane) => {
+          if (panes.length === 0) {
+            $scope.select(pane);
+          } else {
+            pane.selected = false;
+          }
+          panes.push(pane);
+        };
+        $scope.panes = panes;
+      }],
+      templateUrl: 'views/dashboard-tabs.html'
+    };
+  })
+  .directive('leaderBoard', ['$http', $http => (
+    {
+      restrict: 'AE',
+      replace: true,
+      require: '^^dashboardTabs',
+      scope: {
+        title: '@'
+      },
+      link: (scope, element, attribs, tabsController) => {
+        scope.title = 'LeaderBoard';
+        tabsController.addPane(scope);
+        $http.get('api/leaderboard')
+          .then((response) => {
+            const gameWinners = response.data.result.map(record => record.gameWinner);
+            const gameWinnerAggregates = gameWinners.reduce((aggregate, value) => {
+              const index = aggregate.map(x => x.name).indexOf(value);
+              if (index > -1) {
+                aggregate[index].frequency += 1;
+              } else {
+                aggregate.push({
+                  name: value,
+                  frequency: 1
+                });
+              }
+              return aggregate;
+            }, []);
+            // gameWinnerAggregates = gameWinnerAggregates.slice(0);
+            gameWinnerAggregates.sort((a, b) => b.frequency - a.frequency);
+            scope.leaderboard = gameWinnerAggregates;
+          });
+      },
+      templateUrl: 'views/leaderboard.html',
+    })]
+  )
+  .directive('gameHistory', ['$http', $http => (
+    {
+      restrict: 'AE',
+      replace: true,
+      require: '^^dashboardTabs',
+      scope: {
+        title: '@'
+      },
+      link: (scope, element, attribs, tabsController) => {
+        scope.title = 'Game History';
+        tabsController.addPane(scope);
+        $http.get('/api/games/history')
+          .then((response) => {
+            scope.gameHistory = response.data.result;
+          });
+      },
+      templateUrl: 'views/gameHistory.html'
+    })]
+  )
+  .directive('donations', ['$http', $http => (
+    {
+      restrict: 'AE',
+      replace: true,
+      require: '^^dashboardTabs',
+      link: (scope, element, attribs, tabsController) => {
+        scope.title = 'Donations';
+        scope.user = user.name;
+        tabsController.addPane(scope);
+        $http.get('/api/donations')
+          .then((response) => {
+            scope.donations = response.data.result;
+          });
+      },
+      templateUrl: 'views/donations.html'
+    })]
+  )
   .directive('chatbox', ['socket', socket => ({
     restrict: 'AE',
     replace: true,
     link: (scope, element) => {
-       // Send chat message
+      // Send chat message
       scope.sendChatMessage = () => {
         const chat = {};
         chat.message = $('#chatInput').val();
@@ -126,7 +222,7 @@ angular.module('mean.directives', [])
         window.localStorage.setItem('chatcount', 0);
         scope.isPlayerSet = true;
       };
-        // listen for chat messages
+      // listen for chat messages
       socket.on('chat message', (chat) => {
         displayChat(chat);
       });
@@ -137,7 +233,7 @@ angular.module('mean.directives', [])
         hidePickerOnBlur: true
       });
 
-        // Submit the chat when the 'enter' key is pressed
+      // Submit the chat when the 'enter' key is pressed
       $('body').on('keyup', '.emojionearea-editor', (event) => {
         if (event.which === 13) {
           $('.emojionearea-editor').trigger('blur');
